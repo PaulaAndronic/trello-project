1d5061bf3280598ec650883c8a1439279ff9012b
Paula Andronic <paula.andronic@sparktech.ro>
2022-12-19 01:06:57 +0200

    add server on git

--- server/server.js
+++ server/server.js
@@ -27,6 +27,36 @@ app.delete('/', async (request, response) => {
   response.send("Done");
 })
 
+app.put('/', async (request, response) => {
+  const { boardId, title } = request.body;
+  const res = await pool.query('UPDATE boards SET title = $1 WHERE boardid = $2', [title, boardId]);
+  response.send("Done");
+})
+
+app.get('/board/:boardId', async (request, response) => {
+  const res = await pool.query("SELECT * FROM lists WHERE boardid=$1", [request.params.boardId]);
+  response.send(res.rows);
+})
+
+app.post('/board/:boardId', async (request, response) => {
+  const { title } = request.body;
+  const res = await pool.query('INSERT INTO lists(title, boardId) VALUES($1, $2)', [title, request.params.boardId]);
+  response.send("Done");
+})
+
+app.delete('/board/:boardId', async (request, response) => {
+  const { listId } = request.body;
+  console.log(listId);
+  const res = await pool.query('DELETE FROM lists WHERE listid=$1', [listId]);
+  response.send("Done");
+})
+
+app.put('/board/:boardId', async (request, response) => {
+  const { listId, title } = request.body;
+  const res = await pool.query('UPDATE lists SET title = $1 WHERE listid = $2', [title, listId]);
+  response.send("Done");
+})
+
 app.listen(PORT, () => {
   console.log(`🚀 Server started at http://localhost:${PORT}`)
 })
